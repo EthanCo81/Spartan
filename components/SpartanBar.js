7b377218d71303bff68c465d4691ea6ccb8beb74
Ethan Conner <ethanco81@ymail.com>
2018-11-19 14:44:21 -0600

    first commit

---
+++ components/SpartanBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+class SpartanBar extends React.Component{
+
+
+    render(){
+        return(
+        <View>
+
+        </View>
+        );
+    }
+
+    const styles = StyleSheet.create(
+        {
+
+        }
+    );
+
+
+}
